ef22176cc3740eee6f19d92dd1cd4036b570674f
st2diio <st2diio@protonmail.ch>
2022-01-14 15:42:55 +0100

    Adding minimal basic ERC20 contract struct

---
+++ contracts/osERC20.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract osERC20 {
+    string private _name;
+    string private _symbol;
+    
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory){
+        return _symbol;
+    }
+
+    function decimal() public pure returns (uint8){
+        return 18;
+    }
+}
